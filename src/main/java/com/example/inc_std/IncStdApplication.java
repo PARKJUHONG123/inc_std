88a09619abd9f322cd3e89c27f10b0ebe49b6081
PARKJUHONG123 <corallines@naver.com>
2021-03-31 23:33:04 +0900

    initial setting

---
+++ src/main/java/com/example/inc_std/IncStdApplication.java
@@ -0,0 +1,13 @@
+package com.example.inc_std;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncStdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IncStdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/inc_std/IncStdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.inc_std;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncStdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
