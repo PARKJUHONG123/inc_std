346752267e6b0808a9c840ec49af432576f70ec1
210552 <210552@shinsegae.com>
2021-04-29 12:58:47 +0900

    delete unnecessary file

--- fastcampus-example/src/main/java/com/fastcampus/example/domain/repository/BookMetaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.fastcampus.example.domain.repository;
-
-import com.fastcampus.example.domain.entity.BookMeta;
-import com.fastcampus.example.domain.entity.BookSalesInfo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookMetaRepository extends JpaRepository<BookMeta, Long> {
-}
--- fastcampus-example/src/main/java/com/fastcampus/example/domain/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.fastcampus.example.domain.repository;
-
-import com.fastcampus.example.domain.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
